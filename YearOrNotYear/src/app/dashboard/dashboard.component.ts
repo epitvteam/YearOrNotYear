49d821cc6cdb17fd70b3a89b7952d7547b2b0d6f
Hugo Castelli <hugo.castelli@epitech.eu>
2019-03-07 19:44:08 +0100

    DONE: logout

--- YearOrNotYear/src/app/dashboard/dashboard.component.ts
+++ YearOrNotYear/src/app/dashboard/dashboard.component.ts
@@ -18,7 +18,8 @@ export class DashboardComponent implements OnInit {
   quote = 'Loading quote';
   email = 'Loading email';
 
-  constructor(private http: HttpClient, private modalService: NgbModal, private user: UserService, private router: Router) {
+  constructor(private http: HttpClient, private modalService: NgbModal, private user: UserService,
+              private router: Router) {
     this.http.get<any>('assets/json/module.json')
       .subscribe(data => {
         this.items = data;
@@ -29,7 +30,7 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit(): void {
     this.user.getData().subscribe(data => {
-      if(data.status) {
+      if (data.status) {
         this.quote = data.quote;
         this.email = data.email;
       } else {
