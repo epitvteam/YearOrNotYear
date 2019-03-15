cfb1c2bed805545540a6aa43cfedf451dac65651
lepipotron <pierre.herman@epitech.eu>
2019-03-15 18:20:38 +0100

    Design

--- YearOrNotYear/src/app/dashboard/dashboard.component.ts
+++ YearOrNotYear/src/app/dashboard/dashboard.component.ts
@@ -21,12 +21,13 @@ export class DashboardComponent implements OnInit {
 
   constructor(private http: HttpClient, private modalService: NgbModal, private user: UserService,
               private router: Router) {
-    this.http.get<any>('assets/json/module.json')
+    /*this.http.get<any>('assets/json/module.json')
       .subscribe(data => {
         this.items = data;
         this.itemsHave = [];
-      });
-    this.getModulesSubscribed('');
+      });*/
+    this.getModulesSubscribed('auth-f6f274a14de80a2343e2c9b75186a460dbc236c5');
+    this.getModulesNotSubscribed('auth-f6f274a14de80a2343e2c9b75186a460dbc236c5');
   }
 
   ngOnInit(): void {
@@ -69,19 +70,8 @@ export class DashboardComponent implements OnInit {
     }
   }
 
-  getInfos(autologin) {
-    let baseUrl = 'http://intra.epitech.eu/' + autologin + '/user/?format=json';
-    this.http.get<any>(baseUrl)
-      .subscribe(data => {
-        let loginUser = data.login;
-        let firstnameUser = data.firstname;
-        let lastnameUser = data.lastname;
-        var jsonRtn = {'login': loginUser, 'firstname': firstnameUser, 'lastname': lastnameUser};
-      });
-  }
-
   async getDescription(autologin, modulecode, year, city) {
-    let URLb = 'http://intra.epitech.eu/' + autologin + '/module/' + year + '/' + modulecode + '/' + city + '/?format=json';
+    const URLb = 'http://intra.epitech.eu/' + autologin + '/module/' + year + '/' + modulecode + '/' + city + '/?format=json';
     return(await this.http.get(URLb).toPromise());
   }
 
@@ -93,17 +83,19 @@ export class DashboardComponent implements OnInit {
     var datas = await this.http.get<any>(baseUrl).toPromise();
     for (let entry of datas) {
       //REPLACE 2018 BY YEAR
-      if ((entry.status == 'ongoing' || entry.status == 'valid') && entry.scolaryear == 2018) {
+      if ((entry.status == 'ongoing' || entry.status == 'valid' || entry.status == 'fail') && entry.scolaryear == 2018) {
         descriptions = await this.getDescription(autologin, entry.code, entry.scolaryear, entry.codeinstance);
-        JsonInArray = {'name': entry.title, 'scolaryear': entry.scolaryear, 'description': descriptions.description, 'credits': entry.credits};
+        JsonInArray = {'name': entry.title, 'scolaryear': entry.scolaryear, 'description': descriptions.description, 'credits': entry.credits, 'status': entry.status};
         returnArray.push(JsonInArray);
       }
     }
     console.log(returnArray);
+    this.itemsHave = returnArray;
+    return(returnArray);
   }
-  
+
   async getModulesNotSubscribed(autologin) {
-    let baseUrl = 'http://intra.epitech.eu/' + autologin + '/course/filter?format=json';
+    const baseUrl = 'http://intra.epitech.eu/' + autologin + '/course/filter?format=json';
     var returnArray = [];
     var JsonInArray;
     var descriptions;
@@ -115,10 +107,13 @@ export class DashboardComponent implements OnInit {
       //REPLACE 2018 BY YEAR
       if (entry.status == 'notregistered' && entry.scolaryear == 2018) {
         descriptions = await this.getDescription(autologin, entry.code, entry.scolaryear, entry.codeinstance);
-        JsonInArray = {'name': entry.title, 'scolaryear': entry.scolaryear, 'description': descriptions.description, 'credits': entry.credits, 'status': entry.status};
+        JsonInArray = {'name': entry.title, 'scolaryear': entry.scolaryear, 'description': descriptions.description,
+          'credits': entry.credits, 'status': entry.status};
         returnArray.push(JsonInArray);
       }
     }
-    return(JSON.stringify(returnArray));
+    console.log(returnArray);
+    this.items = returnArray;
+    return(returnArray);
   }
 }
